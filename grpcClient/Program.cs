47facb4a4d77916639d8ed62d16112c85cc56f44
hhsynalv <alavhasan72892@gmail.com>
2022-09-06 18:57:38 +0300

    Unary RPC

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,9 +1,21 @@
 ﻿using Grpc.Net.Client;
+using grpcMessageClient;
 using grpcServer;
 
 var channel = GrpcChannel.ForAddress("https://localhost:5001");
-var greetClient = new Greeter.GreeterClient(channel);
 
-HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "Selamlar Efendim." });
+//var greetClient = new Greeter.GreeterClient(channel);
+//HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "Selamlar Efendim." });
+//Console.WriteLine(result.Message);
 
-Console.WriteLine(result.Message);
+
+var messageClient = new Message.MessageClient(channel);
+
+var response = await messageClient.SendMessageAsync(new MessageRequest()
+{
+    Message = "Bu bir mesaj gönderme denemesidir",
+    Name = "beyazskorsky"
+});
+
+Console.WriteLine(response.Message);
+Console.ReadKey();
---
+++ grpcServer/Services/MessageService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using grpcMessageServer;
+
+namespace grpcServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        public override async Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Message : {request.Message} | Name: {request.Name}");
+
+            return new MessageResponse()
+            {
+                Message = "Mesaj başarıyla alınmıştır"
+            };
+        }
+    }
+}
